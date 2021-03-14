3a84bae7c5b769d3e6c4d9fe0ee7089efedd1ad1
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-14 20:25:26 +0300

    init: dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,9 +2,12 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   let CodeName = [];
-  let j = 0;
+  let j = 0, t = 0;
   for(let i = 0;i<members.length;i++) {
     if (typeof members[i] === "string") {
+      if(members[i][0] === ' ') {
+        members[i] = members[i].replace(/\s/g, '');
+      }
       CodeName[j] = members[i][0];
       j++;
     }
