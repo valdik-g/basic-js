6221135deb38ac91561f9d633b13e36a3a88ee53
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-15 21:10:48 +0300

    init:transform.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,26 +2,27 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform( arr ) {
   if(typeof arr === "null" || typeof arr === "undefined"){
-    throw Error;
+    throw new RangeError('Error');
   }
   let NewArr = [];
-  let length = 0;
-  let t = 0;
   NewArr = arr;
-  for(let i = 0;i<arr.length;i++){
+  for(let i = 0;i<NewArr.length;i++){
     if(typeof arr[i]==="string") {
-      if (arr[i] === "--discard-next" && typeof arr[i + 1] !== "undefined") {
+      if (arr[i] === '--discard-next' && typeof arr[i + 1] !== "undefined") {
         NewArr.splice(i,2);
       }
-      if (arr[i] === "--discard-prev" && typeof arr[i - 1] !== "undefined") {
+      if (arr[i] === '--discard-prev' && typeof arr[i - 1] !== "undefined") {
         NewArr.splice(i - 1,2);
       }
-      if (arr[i] === "--double-next" && typeof arr[i + 1] !== "undefined") {
+      if (arr[i] === '--double-next' && typeof arr[i + 1] !== "undefined") {
         NewArr.splice(i,1, arr[i+1]);
       }
-      if (arr[i] === "--double-prev" && typeof arr[i - 1] !== "undefined") {
-        NewArr.splice(i-1,0,arr[i-1])
+      if (arr[i] === '--double-prev' && typeof arr[i - 1] !== "undefined") {
+        NewArr.splice(i,1,arr[i-1])
       }
+      else
+        NewArr.splice(i,1);
+      i--;
     }
   }
   return NewArr;
