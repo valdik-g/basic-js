839d8c52f13174af656beab3e934f1787cc1a37a
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-25 08:19:45 +0300

    init:repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let mas = "";
+    let last = false;
+    if (typeof options.repeatTimes === "undefined")
+        options.repeatTimes = 1;
+    if (typeof options.addition === "undefined")
+        options.addition = "";
+    if (typeof options.additionRepeatTimes === "undefined")
+        options.additionRepeatTimes = 1;
+    if (typeof options.separator === "undefined")
+        options.separator = "+";
+    if (typeof options.additionSeparator === "undefined")
+        options.additionSeparator = "|";
+    for (let i = 0; i < options.repeatTimes; i++) {
+        mas+=str;
+        if(options.additionRepeatTimes == 1) {
+            mas+=String(options.addition);
+        }
+        else{
+            mas+=String(options.addition);
+            mas+=String(options.additionSeparator);
+        }
+        for(let j = 1;j< options.additionRepeatTimes;j++) {
+            if(j === options.additionRepeatTimes - 1) {
+                mas += String(options.addition);
+                last = false;
+            }
+            else{
+                mas += String(options.addition);
+                mas += String(options.additionSeparator);
+                last = true;
+            }
+        }
+        if(last){
+            mas+=String(options.addition);
+        }
+        if(i !== options.repeatTimes - 1)
+            mas+= String(options.separator);
+    }
+    return mas;
 };
   
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let turn = Math.pow(2,disksNumber) - 1;
+    let second = Math.floor(turn*3600/turnsSpeed);
+    let t ={
+    turns: turn, seconds: second
+  };
+  return t;
 };
